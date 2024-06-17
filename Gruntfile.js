e549698566eff9d87dc325a2cc0351677edbc695
libamts <libamtr@liba.com.br>
2024-06-17 12:23:19 -0300

    adicionando projeto

---
+++ Gruntfile.js
@@ -0,0 +1,32 @@
+const { option } = require("grunt");
+
+module.exports = function (grunt){
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        less: {
+            development: {
+                    options: {
+                        compress: true
+                    },
+                    files: {
+                        'dist/style/main.min.css' : 'src/styles/main.less'
+                    }
+                }
+                },
+        uglify: {
+            target: {
+                files: {
+                    'dist/scripts/main.min.js' : 'src/scripts/main.js'
+                }
+            }
+        }
+    })
+
+
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.registerTask('default', ['less:development', 'uglify']);
+}
+
